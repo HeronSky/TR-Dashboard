817445bc2bb2e7291da1faf0179a160214d479b1
海龍在天 <horyun.tw@gmail.com>
2026-03-23 23:02:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,41 @@ import json,folium,branca.colormap as cm,streamlit as st,pandas as pd
 from streamlit_folium import st_folium
 
 st.set_page_config(page_title="TR MAP",layout="wide")
-st.sidebar.title("地圖設定")
-days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-selected_day = st.sidebar.selectbox("選擇日期", days)
-st.title(f"TR MAP ({selected_day})")
 
-m = folium.Map(location=[23.5,121.0],zoom_start=7,tiles="CartoDB positron")
+st.markdown(
+    """
+    <style>
+        header {visibility: hidden;}
+        #MainMenu {visibility: hidden;}
+        
+        .block-container {
+            padding-top: 0rem !important;
+            padding-bottom: 0rem !important;
+            padding-left: 0rem !important;
+            padding-right: 0rem !important;
+            max-width: 100% !important;
+        }
+    
+        [data-testid="stSidebar"] {
+            min-width: 350px;
+            max-width: 350px;
+        }
+        [data-testid="stAppViewContainer"], [data-testid="stMainBlockContainer"] {
+            overflow: hidden !important;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+selected_day = "Monday"
+days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+st.sidebar.title("TR MAP")
+selected_day = st.sidebar.selectbox("選擇日期",days)
+st.sidebar.divider()
+
+
+m = folium.Map(location=[23.5,121.0],zoom_start=8,tiles="CartoDB dark_matter nolabels")
 
 with open("data/台鐵全部時刻表.json",'r',encoding='utf-8') as f:
     schedule_data = json.load(f)
@@ -58,43 +87,6 @@ for timetable in schedule_data['TrainTimetables']:
         
 print(TimeTables)
 
-
-with open('data/車站基本資料.json','r',encoding='utf-8') as f:
-    station_data = json.load(f)
-
-for station in station_data:
-    station_name = station['StationName']['Zh_tw']
-    lat = station['StationPosition']['PositionLat']
-    lon = station['StationPosition']['PositionLon']
-
-    freq = TimeTables.get(station_name,0)
-    if freq >= 150:
-        color = 'red'
-        radius = 6
-    elif freq >= 100:
-        color = 'orange'
-        radius = 5
-    elif freq >= 50:
-        color = 'yellow'
-        radius = 4
-    elif freq > 0:
-        color = 'green'
-        radius = 3
-    else: 
-        continue
-        
-    folium.CircleMarker(
-        location=[lat,lon],
-        radius=radius,
-        color=color,
-        fill=True,
-        fill_color=color,
-        fill_opacity=0.7,
-        tooltip=station_name,
-        popup=f"{station_name}: {freq} trains"
-    ).add_to(m)
-
-
 with open('data/台鐵線型.json','r',encoding='utf-8') as f:
     shape_data = json.load(f)
 
@@ -119,30 +111,70 @@ for shape in shape_data['Shapes']:
         if len(track_path) >= 2:
             folium.PolyLine(
                 locations=track_path,
-                color='red',
+                color='#335577',
                 weight=2,
                 opacity=0.5,
             ).add_to(m)
 
-col1, col2 = st.columns([2, 1])
-with col1:
-    map_data = st_folium(m, width=700, height=500)
-with col2:
-    user_clicked_station = None
 
-    if map_data and map_data.get("last_object_clicked_tooltip"):
-        user_clicked_station = map_data["last_object_clicked_tooltip"]
-    
-    if user_clicked_station and user_clicked_station in id_name_map:
+with open('data/車站基本資料.json','r',encoding='utf-8') as f:
+    station_data = json.load(f)
+
+for station in station_data:
+    station_name = station['StationName']['Zh_tw']
+    lat = station['StationPosition']['PositionLat']
+    lon = station['StationPosition']['PositionLon']
+
+    freq = TimeTables.get(station_name,0)
+    if freq >= 150:
+        color = '#FF0055'
+        radius = 6
+    elif freq >= 100:
+        color = '#FF9900'
+        radius = 5
+    elif freq >= 50:
+        color = '#00FFCC'
+        radius = 4
+    elif freq > 0:
+        color = '#666666'
+        radius = 3
+    else: 
+        continue
+        
+    folium.CircleMarker(
+        location=[lat,lon],
+        radius=radius,
+        color=color,
+        fill=True,
+        fill_color=color,
+        fill_opacity=0.7,
+        tooltip=station_name,
+        popup=f"{station_name}: {freq} trains"
+    ).add_to(m)
+
+
+user_clicked_station = None
+map_data = st_folium(m,use_container_width=True,height=900)
+if map_data and map_data.get("last_object_clicked_tooltip"):
+    user_clicked_station = map_data["last_object_clicked_tooltip"]
+
+if user_clicked_station and user_clicked_station in id_name_map:
+        
+    with st.sidebar:
+        st.divider()    
         clicked = id_name_map[user_clicked_station]
         if clicked in StationInfo:
             info_list = StationInfo[clicked]
             
             schedule_table = pd.DataFrame(info_list)
+            st.write(f"### {user_clicked_station}站")
+            st.write(f"總計 {len(info_list)} 班列車")
+            choice = st.selectbox("方向：",["全部","順行","逆行"])
+            if choice != "全部":
+                schedule_table = schedule_table[schedule_table["方向"] == choice]
             schedule_table = schedule_table.sort_values(by="時間")
-            st.write(f"### {user_clicked_station}站 時刻表")
             st.dataframe(schedule_table, hide_index=True, use_container_width=True, height=500)
-    
+
 
 
 
